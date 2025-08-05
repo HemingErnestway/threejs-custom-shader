112f2ff161537e779c6f395602d7ffdcc510779a
Anton Smirnov <ernestwayh@gmail.com>
2025-08-05 12:05:22 +0300

    feat: fog affecting shader material

--- src/app/threejs-scene.jsx
+++ src/app/threejs-scene.jsx
@@ -3,6 +3,7 @@
 "use client";
 
 import gsap from "gsap";
+import * as THREE from "three";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
@@ -11,18 +12,29 @@ import { OrbitControls } from "@react-three/drei";
 function Cube() {
   const vertexShader = `
     varying vec3 vGlobalPosition;
+    
+    ${THREE.ShaderChunk["common"]}
+    ${THREE.ShaderChunk["fog_pars_vertex"]}
 
     void main() {
+      ${THREE.ShaderChunk["begin_vertex"]}
+      ${THREE.ShaderChunk["project_vertex"]}
+    
       vec4 globalPosition = modelMatrix * vec4(position, 1.0);
       vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
       
       vGlobalPosition = globalPosition.xyz;
       gl_Position = projectionMatrix * modelViewPosition; 
+      
+      ${THREE.ShaderChunk["fog_vertex"]}
     }
   `;
 
   const fragmentShader = `
     varying vec3 vGlobalPosition;
+    
+    ${THREE.ShaderChunk["common"]}
+    ${THREE.ShaderChunk["fog_pars_fragment"]}
     
     void main() {
       float r = (vGlobalPosition.x + 0.5) / 5.0;
@@ -30,6 +42,8 @@ function Cube() {
       float b = (vGlobalPosition.z + 0.5) / 5.0;
       
       gl_FragColor = vec4(r, g, b, 1.0);
+      
+      ${THREE.ShaderChunk["fog_fragment"]}
     }
   `;
 
@@ -49,8 +63,10 @@ function Cube() {
     <mesh ref={cubeRef}>
       <boxGeometry args={[1, 1, 1]} />
       <shaderMaterial
+        uniforms={THREE.UniformsUtils.merge([THREE.UniformsLib["fog"]])}
         vertexShader={vertexShader}
         fragmentShader={fragmentShader}
+        fog={true}
       />
     </mesh>
   );
@@ -58,9 +74,10 @@ function Cube() {
 
 export default function ThreejsScene() {
   return (
-    <Canvas style={{ background: "black" }}>
+    <Canvas style={{ background: "#333333" }}>
       <Cube />
       <OrbitControls />
+      <fog attach="fog" args={[0x333333, 10, 15]}/>
       <axesHelper />
     </Canvas>
   );
